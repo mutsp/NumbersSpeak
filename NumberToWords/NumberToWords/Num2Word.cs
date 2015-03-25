4908ed8c498e6b9f0f80078dc9fefeb90a38dce3
Mutungire Peter <mutungirep@yahoo.com>
2015-03-25 16:51:38 +0300

    Convert Numbers to words

---
+++ NumberToWords/NumberToWords/Num2Word.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NumberToWords
+{
+   public class Num2Word
+    {
+
+        public  string NumberToWords(int number)
+        {
+            string words = System.String.Empty;
+
+            string[] onesMap = new string[] { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+
+            string[] tensMap = new string[] { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+          
+            if (number == 0)
+            {
+                return "Zero";
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += NumberToWords(number / 1000) + " Thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += NumberToWords(number / 100) + " Hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+     
+
+                if (number < 20)
+                {
+                    words += onesMap[number];
+                }
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                    {
+                        words += "-" + onesMap[number % 10];
+                    }
+                }
+            }
+
+            return words;
+        }
+    }
+}
---
+++ NumberToWords/NumberToWords/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace NumberToWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Num2Word numberToword = new Num2Word();
+            Console.Write("NUMBERS SPEAK Console Appliccation." + "\n\n");
+            Console.Write("Enter the Number to Convert.." + "\n");
+            string line;
+            int ln1,value;
+            do
+            {
+                Console.Write(" ");
+                line = Console.ReadLine();
+                if (line == null)
+                {
+                    Console.WriteLine("Please enter a Number");                  
+                }
+                else
+                {
+                    if (int.TryParse(line, out value))
+                    {
+                        ln1 = int.Parse(line);
+                        if (ln1 < 0 || ln1 > 4500)
+                        {
+                        Console.WriteLine("Please enter a number between 0 and 4500");
+                        }
+                        else
+                        {
+                            Console.WriteLine("   " + numberToword.NumberToWords(ln1));
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please only integer Numbers are allowed..");
+                    }
+                }
+            } 
+            while (line != null);  
+        }
+
+    }
+}
